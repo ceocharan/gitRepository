ee27ba29c86ac6e86fc6e9ce767b3b0068685177
ceocharan <sricharan0871@gmail.com>
2024-11-02 22:02:10 +0530

    f1

--- Gt.java
+++ Gt.java
@@ -1,5 +1,6 @@
 public class Gt {
     void m1(){
         System.out.println("m2");
+        System.out.println("25");
     }
 }
--- Main.java
+++ Main.java
@@ -3,7 +3,4 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    void m1(){
-        System.out.println("M1");
-    }
 }
